1fb7602ba5480c96e6f42239340815e73c401cca
Philip Stevens <philip.stevens@reevoo.com>
2014-04-24 00:00:13 +0100

    Reflected the fact that the app tests really belonged to route tests.

--- test/routes_test.js
+++ test/routes_test.js
@@ -1,21 +1,14 @@
 "use strict";
+
 var request = require('supertest')
   , assert = require('assert')
   , app = require('../app');
 
-describe("app", function() {
+describe("routes", function() {
 
   describe('/reevoomark/track/impression', function(){
     it("responds with a 200", function(done) {
       request(app).get("/reevoomark/track/impression").expect(200, done);
-    });
-
-    it("publishes to rabbitmq", function() {
-
-    });
-
-    it("publishes expected data to rabbitmq", function(){
-
     });
   });
 
@@ -23,28 +16,11 @@ describe("app", function() {
     it("responds with a 200", function(done) {
       request(app).get("/reevoomark/track/impression").expect(200, done);
     });
-
-    it("publishes to rabbitmq", function() {
-
-    });
-
-    it("publishes expected data to rabbitmq", function(){
-
-    });
-
   });
 
   describe('/reevoomark/track/miss', function(){
     it("responds with a 200", function(done) {
       request(app).get("/reevoomark/track/impression").expect(200, done);
-    });
-
-    it("publishes to rabbitmq", function() {
-
-    });
-
-    it("publishes expected data to rabbitmq", function(){
-
     });
   });
 });
