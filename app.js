afb1f18074e2b72fc7d8afa7a2ed417cc1fe468c
Philip Stevens <philip.stevens@reevoo.com>
2014-06-26 16:11:19 +0100

    Improved example and refactored into module mode with tests.

--- app.js
+++ app.js
@@ -5,98 +5,19 @@
 var morgan = require('morgan')
   , express = require('express')
   , argv = require('minimist')(process.argv.slice(2))
-  , fs = require('fs')
-  , Starling = require('./lib/starling')
+  , logStream = require('./lib/logStream')
+  , server = require('./lib/server')
+  , Starling = require('./starling')
   , app = express();
 
-// Usage variables
-var port = process.env.PORT || argv.port
-  , logFile = process.env.LOG || argv.logFile
-  , starlingHosts = argv._
-  , imageResponse = fs.readFileSync('./lib/tracker.gif')
-  , responseHeaders = {
-      "Content-Type": "image/gif",
-      "Cache-Control": "no-cache, no-store, max-age=0, must-revalidate",
-      "Pragma": "no-cache",
-      "Expires": "Fri, 29 Aug 1997 02:14:00 EST"
-    };
+var port   = process.env.PORT || argv.port
+  , stream = logStream(process.env.LOG || argv.log)
+  , starling = new Starling(argv._);
 
-console.log(starlingHosts);
-
-// initialize the Starling queues
-var starling = new Starling(starlingHosts);
-
-// initialize the logStream and apply refresh on SIGHUP
-var logStream;
-if (logFile) {
-  logStream = fs.createWriteStream(logFile, { flags: 'a' });
-  process.on('SIGHUP', function () {
-      log("SIGHUP: Reopening logfile");
-      logStream.end();
-      logStream = fs.createWriteStream(logFile, { flags: 'a' });
-      log("SIGHUP: Starting new log file");
-    }
-  );
-} else {
-  logStream = process.stdout;
-}
-app.use(morgan({ format: 'tiny', stream: logStream }));
+app.use(morgan({ format: 'tiny', stream: stream }));
+server(app, starling);
 
 app.enable('trust proxy');
-app.get('/reevoomark/track/miss', function (req, res) {
-    var body = JSON.stringify({
-        requested_at: new Date().toString(),
-        request_params: { sku: req.query.sku },
-        retailer_id: req.query.retailer_id,
-        badge_type: req.query.badge_type,
-        badge_variant: req.query.badge_variant
-      }
-    );
-
-    starling.push('super_poller', { name: 'miss', body: body }, function () {
-      res.status(200).set(responseHeaders).send(imageResponse);
-    });
-  }
-);
-
-app.get('/reevoomark/track/impression', function (req, res) {
-    var body = JSON.stringify({
-        requested_at: new Date().toString(),
-        product_id: req.query.product_id,
-        retailer_id: req.query.retailer_id,
-        badge_type: req.query.badge_type,
-        badge_variant: req.query.badge_variant,
-        badge_name: req.query.badge_name
-      }
-    );
-
-
-    starling.push('super_poller', { name: 'impression', body: body }, function () {
-      res.status(200).set(responseHeaders).send(imageResponse);
-    });
-  }
-);
-
-app.get('/reevoomark/track/non_impression', function (req, res) {
-    var body = JSON.stringify({
-        requested_at: new Date().toString(),
-        product_id: req.query.product_id,
-        retailer_id: req.query.retailer_id,
-        badge_type: req.query.badge_type,
-        badge_variant: req.query.badge_variant
-      }
-    );
-
-
-    starling.push('super_poller', { name: 'non_impression', body: body }, function () {
-      res.status(200).set(responseHeaders).send(imageResponse);
-    });
-  }
-);
-
-app.listen(port, function () {
-    console.log('Server listening on port %d', port);
-  }
-);
+app.listen(port, function () { console.log('Server listening on port %d', port); });
 
 exports = module.exports = app;
---
+++ test/server_test.js
@@ -0,0 +1,123 @@
+"use strict";
+
+var should = require('should')
+  , server = require('../lib/server')
+  , express = require('express')
+  , request = require('supertest');
+
+describe("The server", function () {
+  var fakeStarling, app;
+
+  beforeEach(function () {
+    app = express();
+    fakeStarling = {
+      push: function (queue, message, callback) {
+        this.received.push([queue, message]);
+        callback();
+      },
+      received: []
+    };
+
+    server(app, fakeStarling)
+    app.now = function(){
+      return "the-current-time";
+    };
+  });
+
+  describe("default responses", function () {
+    it("responds with a 200", function (done) {
+      request(app).get("/reevoomark/track/impression?product_id=bar").expect(200, done);
+    });
+
+    it("responds with a gif file", function (done) {
+      request(app)
+        .get("/reevoomark/track/impression?product_id=bar")
+        .expect("Content-Type", 'image/gif', done);
+    });
+
+    it("queues a message on the super_poller", function (done) {
+      request(app)
+        .get("/reevoomark/track/impression?product_id=bar")
+        .end(function (val) {
+          fakeStarling.received.length.should.equal(1);
+          fakeStarling.received[0][0].should.equal('super_poller');
+          JSON.stringify(fakeStarling.received[0][1]).should.equal(
+            JSON.stringify({name: 'impression', body: {requested_at: "the-current-time", product_id: 'bar'}})
+          );
+          done(val);
+        });
+    });
+
+    it("responds with a gif file", function (done) {
+      request(app)
+        .get("/reevoomark/track/impression?product_id=bar")
+        .expect("Content-Type", 'image/gif')
+        .end(done);
+    });
+  });
+
+  function theMessage () {
+    return fakeStarling.received[0][1];
+  }
+
+  describe("GET /reevoomark/track/miss", function () {
+    it("Pushes a message onto the queue", function (done) {
+      request(app)
+        .get("/reevoomark/track/miss?sku=SKU&retailer_id=1&badge_type=BADGE_TYPE&badge_variant=BADGE_VARIANT")
+        .end(function (val) {
+          theMessage().should.eql({
+            name: 'miss',
+            body: {
+              requested_at: 'the-current-time',
+              retailer_id: '1',
+              badge_type: 'BADGE_TYPE',
+              badge_variant: 'BADGE_VARIANT',
+              request_params: {sku: 'SKU'}
+            }
+          });
+          done(val);
+        });
+    });
+  });
+
+  describe("GET /reevoomark/track/impression", function () {
+    it("Pushes a message onto the queue", function (done) {
+      request(app)
+        .get("/reevoomark/track/impression?product_id=99&retailer_id=1&badge_type=BADGE_TYPE&badge_variant=BADGE_VARIANT&badge_name=BADGE_NAME")
+        .end(function (val) {
+          theMessage().should.eql({
+            name: 'impression',
+            body: {
+              requested_at: 'the-current-time',
+              product_id: '99',
+              retailer_id: '1',
+              badge_type: 'BADGE_TYPE',
+              badge_variant: 'BADGE_VARIANT',
+              badge_name: 'BADGE_NAME'
+            }
+          });
+          done(val);
+        });
+    });
+  });
+
+  describe("GET /reevoomark/track/non_impression", function () {
+    it("Pushes a message onto the queue", function (done) {
+      request(app)
+        .get("/reevoomark/track/non_impression?product_id=99&retailer_id=1&badge_type=BADGE_TYPE&badge_variant=BADGE_VARIANT")
+        .end(function (val) {
+          theMessage().should.eql({
+            name: 'non_impression',
+            body: {
+              requested_at: 'the-current-time',
+              product_id: '99',
+              retailer_id: '1',
+              badge_type: 'BADGE_TYPE',
+              badge_variant: 'BADGE_VARIANT'
+            }
+          });
+          done(val);
+        });
+    });
+  });
+});
