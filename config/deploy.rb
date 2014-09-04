0b2e8d3137f100274cb2a187edf964f432541a69
Philip Stevens <philip.stevens@reevoo.com>
2014-09-04 14:24:09 +0100

    Use NPM node_modules directory and add the node_module directory to the branch.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,21 +1,16 @@
-# config valid only for Capistrano 3.1
 lock '3.2.1'
 
 set :application, 'tracking-node'
 set :repo_url, 'git@github.com:tuisco/tracking-node.git'
-
-# Default branch is :master
+set :deploy_to, "/apps/#{fetch(:application)}"
+set :npm_target_path, -> { release_path.join('node_modules') }
 ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 
-# Default deploy_to directory is /var/www/my_app
-set :deploy_to, '/apps/mark-tracking-node'
-
 namespace :deploy do
-
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      execute :sudo, 'restart mark-tracking-node'
+
     end
   end
 
