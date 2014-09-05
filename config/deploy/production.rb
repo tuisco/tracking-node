7bf31c584a480e6832a9c9909ab8c0b9c706699b
Philip Stevens <philip.stevens@reevoo.com>
2014-09-05 13:00:48 +0100

    Test in Travis CI and provide default examples of hostname files.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,45 +1,19 @@
-# Simple Role Syntax
-# ==================
-# Supports bulk-adding hosts to roles, the primary server in each group
-# is considered to be the first unless any hosts have the primary
-# property set.  Don't declare `role :all`, it's a meta role.
+fetch(:hostnames).each do |host|
+  server host, user: 'conan', roles: %w{web app}
+end
 
-role :app, %w{deploy@example.com}
-role :web, %w{deploy@example.com}
-role :db,  %w{deploy@example.com}
+namespace :deploy do
+  desc 'Restart monit'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      exec "sudo restart #{fetch(:application)} || sudo start #{fetch(:application)}"
+    end
+  end
 
-
-# Extended Server Syntax
-# ======================
-# This can be used to drop a more detailed server definition into the
-# server list. The second argument is a, or duck-types, Hash and is
-# used to set extended properties on the server.
-
-server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
-
-
-# Custom SSH Options
-# ==================
-# You may pass any option but keep in mind that net/ssh understands a
-# limited set of options, consult[net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start).
-#
-# Global options
-# --------------
-#  set :ssh_options, {
-#    keys: %w(/home/rlisowski/.ssh/id_rsa),
-#    forward_agent: false,
-#    auth_methods: %w(password)
-#  }
-#
-# And/or per server (overrides global)
-# ------------------------------------
-# server 'example.com',
-#   user: 'user_name',
-#   roles: %w{web app},
-#   ssh_options: {
-#     user: 'user_name', # overrides user setting above
-#     keys: %w(/home/user_name/.ssh/id_rsa),
-#     forward_agent: false,
-#     auth_methods: %w(publickey password)
-#     # password: 'please use keys'
-#   }
+  desc 'Link required files'
+  task :link_files do
+    on roles(:app) do
+      exec "ln -s #{shared_path.join('newrelic.js')} newrelic.js"
+    end
+  end
+end
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,1 +1,13 @@
-role :app, %w{conan@staging14.reevoocloud.com}
+fetch(:hostnames).each do |host|
+  server host, user: 'conan', roles: %w{web app}
+end
+
+namespace :deploy do
+  desc 'Restart monit'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      # TODO: restart application
+      # exec "sudo restart #{fetch(:application)} || sudo start #{fetch(:application)}"
+    end
+  end
+end
