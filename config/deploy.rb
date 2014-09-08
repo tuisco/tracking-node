bea3b08c59f73da2805a608aa400e86ee106708b
Philip Stevens <philip.stevens@reevoo.com>
2014-09-08 12:34:36 +0100

    Fix name of tracking node

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 lock '3.2.1'
 require 'yaml'
 
-set :application, 'tracking-node'
+set :application, 'tracking_node'
 set :repo_url, 'git@github.com:tuisco/tracking-node.git'
 set :deploy_to, "/apps/#{fetch(:application)}"
 set :npm_target_path, -> { release_path.join('node_modules') }
